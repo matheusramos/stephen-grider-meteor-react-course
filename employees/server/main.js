959934af183a72e9bcc417cc35c524b55b0b6680
Matheus Ramos <matheus.ramos@icarotech.com>
2016-10-25 14:02:13 -0200

    Section 3 completed

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,5 +1,31 @@
+// only executed on the server
+
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // great place to generate data
+
+  // check to see if data exists in collections
+  // see if collection has any records
+  const numberRecords = Employees.find({}).count();
+
+  if (!numberRecords) {
+    // generate some data
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    });
+  }
+
+  // gets a limited number of employees
+  Meteor.publish('employees', (per_page) => {
+    return Employees.find({}, { limit: per_page });
+  });
 });
